405285d5d3b014bc01b7df90607d5cab3aae322b
Preetam Chinde <75744062+prem1601@users.noreply.github.com>
2024-10-16 13:00:57 +0530

    Check PWA install

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,8 +3,31 @@ import Sidebar from "@/components/Sidebar";
 import "@/styles/globals.css";
 import { ThemeProvider } from "next-themes";
 import type { AppProps } from "next/app";
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    let deferredPrompt: any;
+
+    const handleBeforeInstallPrompt = (e: any) => {
+      // Prevent the mini info bar from appearing
+      e.preventDefault();
+      deferredPrompt = e; // Stash the event
+
+      // Here, instead of showing a button, you can log or handle silently
+      console.log("Install prompt available");
+    };
+
+    window.addEventListener("beforeinstallprompt", handleBeforeInstallPrompt);
+
+    return () => {
+      window.removeEventListener(
+        "beforeinstallprompt",
+        handleBeforeInstallPrompt
+      );
+    };
+  }, []);
+
   return (
     <ThemeProvider attribute="class">
       <div className="grid grid-cols-12 gap-6 px-5 xl:px-48 my-14 sm:px-20 md:px-32 lg:px-20">
